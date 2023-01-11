002040bae8c69235e917d17eda3c8524881321de
yeram.kim <rksk0718@gmail.com>
2023-01-11 13:06:13 +0900

    commit

--- SportsMonitoring/ex.py
+++
@@ -1,6 +0,0 @@
-import re
- 
-text = "6.4(72)"
- 
-text = text.split("(")[1][:-1]
-print(text)
--- clientapivue/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    devServer: {
-        proxy: 'http://kr.lgtvsdp.com'
-    }
-}
